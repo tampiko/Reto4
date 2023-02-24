194ade6c2d6c04aaf6af34161923a159fb8b31d0
Juan Manuel Herrera Carrera <juan.herrera@cobatab.edu.mx>
2023-02-24 15:44:54 -0600

    Cuentra Regresiva Terminada

--- 04-cuentaRegresiva/cuenta.js
+++ 04-cuentaRegresiva/cuenta.js
@@ -1,8 +1,8 @@
-const fechaDestino = new Date("03/20/2023");
-const divDias = document.querySelector("#days");
-const divHoras = document.querySelector("#hours");
-const divMinutos = document.querySelector("#minutes");
-const divSegundos = document.querySelector("#seconds");
+oculta("alertfechaDestino");
+oculta("alertEvento");
+
+let fechaDestino;
+let evento;
 const miliASegundos = 1000;
 const miliAMinutos = miliASegundos * 60;
 const miliAHoras = miliAMinutos * 60;
@@ -14,25 +14,42 @@ function updCountdown() {
     return;
   }
   const hoy = new Date();
-  const cuantoTiempo = fechaDestino - hoy;
-  const REMAINING_DAYS = Math.floor(cuantoTiempo / miliADias);
-  const REMAINING_HOURS = Math.floor((cuantoTiempo % miliADias) / miliAHoras);
-  const REMAINING_MINUTES = Math.floor(
-    (cuantoTiempo % miliAHoras) / miliAMinutos
-  );
-  const REMAINING_SECONDS = Math.floor(
-    (cuantoTiempo % miliAMinutos) / miliASegundos
-  );
+  const cuantoTiempo = this.fechaDestino - hoy;
+  const losDias = Math.floor(cuantoTiempo / miliADias);
+  const lasHoras = Math.floor((cuantoTiempo % miliADias) / miliAHoras);
+  const losMinutos = Math.floor((cuantoTiempo % miliAHoras) / miliAMinutos);
+  const losSegundo = Math.floor((cuantoTiempo % miliAMinutos) / miliASegundos);
 
-  divDias.textContent = REMAINING_DAYS;
-  divHoras.textContent = REMAINING_HOURS;
-  divMinutos.textContent = REMAINING_MINUTES;
-  divSegundos.textContent = REMAINING_SECONDS;
+  document.querySelector("#days").textContent = losDias;
+  document.querySelector("#hours").textContent = lasHoras;
+  document.querySelector("#minutes").textContent = losMinutos;
+  document.querySelector("#seconds").textContent = losSegundo;
+  document.querySelector("#tableEvento").textContent = `para ${this.evento}`;
 }
 
-// updCountdown();
-
 const comenzar = () => {
+  error = false;
+  oculta("alertfechaDestino");
+  oculta("alertEvento");
+  this.evento = document.querySelector("#evento").value;
+  fDestino = document.querySelector("#fechaDestino");
+  fechaok = new Date(fDestino.value);
+  if (!this.evento) {
+    muestra("alertEvento");
+    error = true;
+  }
+  if (fechaok == "Invalid Date") {
+    muestra("alertfechaDestino");
+    error = true;
+  }
+
+  if (error) {
+    return;
+  }
+
+  fechaok.setHours(0, 0, 0, 0);
+  fechaok.setDate(fechaok.getDate() + 1);
+  this.fechaDestino = fechaok;
   activo = true;
   this.updCountdown();
   setInterval(updCountdown, miliASegundos);
@@ -41,5 +58,3 @@ const comenzar = () => {
 const detener = () => {
   activo = false;
 };
-
-oculta("alertfechaDestino");
