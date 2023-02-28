eb8016dcb94df44ebba5541030a652b3aaf23fda
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2023-02-28 07:03:35 -0600

    Ajustes

--- 01-calculadoraDeIngresos/calculadora.js
+++ 01-calculadoraDeIngresos/calculadora.js
@@ -1,22 +1,15 @@
 console.log("Script Cargado");
-
-// require('../scripts.js');
-// import { validaNumericos } from "../scripts.js";
-
 let pedido = [];
-
 const clean = () => {
   pedido = [];
   dibujaTabla();
   document.querySelector("#total").innerHTML = "";
   document.querySelector("#cuerpoTabla").innerHTML = "";
 };
-
 const agregaProducto = (nombreProducto, precio) => {
   pedido.push({ nombreProducto, precio });
   dibujaTabla();
 };
-
 const revisaProducto = () => {
   error = false;
   oculta('alertNombreProducto')
@@ -32,43 +25,30 @@ const revisaProducto = () => {
     muestra('alertNombreProducto')
     return;
   }
-
-  if (error) {
-    return;
-  }
-
-
-
+  if (error) { return; }
   agregaProducto(nombreProducto, parseFloat(precio));
   document.querySelector("#nombreProducto").value = '';
   document.querySelector("#precio").value = '';
 };
-
 const dibujaTabla = () => {
   let body = document.querySelector("#cuerpoTabla");
   body.innerHTML = "";
   total = 0;
   for (i = 0; i < pedido.length; i++) {
     let tr = document.createElement("tr");
-
     let tdP = document.createElement("td");
     tdP.textContent = pedido[i].nombreProducto;
     tr.appendChild(tdP);
-
     let tdC = document.createElement("td");
     tdC.textContent = pedido[i].precio;
     tr.appendChild(tdC);
-
     body.appendChild(tr);
-
     total += pedido[i].precio;
   }
   let tr = document.createElement("tr");
-
   let tdP = document.createElement("td");
   tdP.textContent = "Total :";
   tr.appendChild(tdP);
-
   let tdC = document.createElement("td");
   tdC.textContent = total;
   tr.appendChild(tdC);
--- 08-agendaTelefonica/agenda.js
+++ 08-agendaTelefonica/agenda.js
@@ -1,3 +1,20 @@
 console.log('Agenta Loaded!');
 oculta("alertNombre");
 oculta("alertTelefono");
+
+agenda = [
+    { nombre: 'Juan', telefono: '923923' }
+];
+
+getData = () => {
+    nombre = document.querySelector('#nombre').value
+    telefono = document.querySelector('#telefono').value
+    console.log('nombre ', nombre);
+    console.log('telefono ', telefono);
+}
+
+pintaTabla = () => {
+    agenda.forEach((usr) => {
+        console.log(usr);
+    });
+}
