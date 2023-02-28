7363f443f0a2e50cab365dfb1c751c350787f819
Juan Manuel Herrera Carrera <juan.herrera@cobatab.edu.mx>
2023-02-28 10:51:54 -0600

    Agenda Completada

--- 08-agendaTelefonica/agenda.js
+++ 08-agendaTelefonica/agenda.js
@@ -1,20 +1,50 @@
-console.log('Agenta Loaded!');
+console.log("Agenta Loaded!");
 oculta("alertNombre");
 oculta("alertTelefono");
 
-agenda = [
-    { nombre: 'Juan', telefono: '923923' }
-];
+agenda = [];
 
 getData = () => {
-    nombre = document.querySelector('#nombre').value
-    telefono = document.querySelector('#telefono').value
-    console.log('nombre ', nombre);
-    console.log('telefono ', telefono);
-}
+  nombre = document.querySelector("#nombre").value;
+  telefono = document.querySelector("#telefono").value;
+  console.log("nombre ", nombre);
+  console.log("telefono ", telefono);
+  if (!nombre || !telefono) {
+    muestra("alertNombre");
+    muestra("alertTelefono");
+    return;
+  }
+  agenda.push({ nombre, telefono });
+  limpia();
+  pintaTabla();
+};
 
 pintaTabla = () => {
-    agenda.forEach((usr) => {
-        console.log(usr);
-    });
-}
+  cuerpo = document.querySelector("#tableBody");
+  codigo = "";
+  agenda.forEach((usr) => {
+    codigo += `
+        <tr>
+            <td>${usr.nombre}</td>
+            <td>${usr.telefono}</td>
+        </tr>
+        `;
+    console.log(usr);
+  });
+  cuerpo.innerHTML = codigo;
+};
+
+limpia = () => {
+  oculta("alertNombre");
+  oculta("alertTelefono");
+  document.querySelector("#nombre").value = "";
+  document.querySelector("#telefono").value = "";
+};
+
+reiniciar = () => {
+  limpia();
+  agenda = [];
+  pintaTabla();
+};
+
+pintaTabla();
